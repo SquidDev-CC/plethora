e1d0c300cf28d6040d4b993655e574b3a0ba25e4
SquidDev <bonzoweb@hotmail.co.uk>
2016-08-07 21:26:48 +0100

    Add builders for module methods
    Have not implemented the "object" style methods

--- src/main/java/org/squiddev/plethora/api/module/TargetedModuleMethod.java
+++ src/main/java/org/squiddev/plethora/api/module/TargetedModuleMethod.java
@@ -2,9 +2,15 @@ package org.squiddev.plethora.api.module;
 
 import net.minecraft.util.ResourceLocation;
 import org.squiddev.plethora.api.method.IContext;
+import org.squiddev.plethora.api.method.IMethod;
 import org.squiddev.plethora.api.method.ISubTargetedMethod;
+import org.squiddev.plethora.api.method.IUnbakedContext;
 
 import javax.annotation.Nonnull;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * A module method that targets a separate class
@@ -38,5 +44,73 @@ public abstract class TargetedModuleMethod<T> extends ModuleMethod implements IS
 	@Override
 	public Class<T> getSubTarget() {
 		return klass;
+	}
+
+	/**
+	 * Delegate to a normal method from a {@link ModuleMethod}.
+	 *
+	 * The method should be a public and static with the same signature as {@link ModuleMethod#apply(IUnbakedContext, Object[])}.
+	 * This does not allow fine grain control over whether a method can be applied or not. If you require
+	 * {@link IMethod#canApply(IContext)} you should use a normal {@link IMethod} instead.
+	 *
+	 * Use {@link #modId()} instead of {@link net.minecraftforge.fml.common.Optional.Method} if you require a mod to
+	 * be loaded. This allows us to blacklist mods in the config.
+	 */
+	@Target(ElementType.METHOD)
+	@Retention(RetentionPolicy.RUNTIME)
+	public @interface Inject {
+		/**
+		 * The name this function should be exposed as.
+		 *
+		 * This defaults to the method's name
+		 *
+		 * @return The function's name
+		 * @see IMethod#getName()
+		 */
+		String name() default "";
+
+		/**
+		 * The module this method targets.
+		 *
+		 * @return The target class.
+		 */
+		String module();
+
+		/**
+		 * The class this method targets
+		 *
+		 * @return The target class
+		 */
+		Class<?> target();
+
+		/**
+		 * The priority of the method.
+		 *
+		 * {@link Integer#MIN_VALUE} is the lowest priority and {@link Integer#MAX_VALUE} is the highest. Methods
+		 * with higher priorities will be preferred.
+		 *
+		 * @return The method's priority
+		 * @see IMethod#getPriority()
+		 */
+		int priority() default 0;
+
+		/**
+		 * The method's doc string.
+		 *
+		 * See {@link IMethod#getDocString()} for format information
+		 *
+		 * @return The method's doc string
+		 * @see IMethod#getDocString()
+		 */
+		String doc() default "";
+
+		/**
+		 * Set if this method depends on a mod
+		 *
+		 * @return The mod's id
+		 * @see net.minecraftforge.fml.common.Optional.Method
+		 * @see net.minecraftforge.fml.common.Optional.Interface
+		 */
+		String modId() default "";
 	}
 }
