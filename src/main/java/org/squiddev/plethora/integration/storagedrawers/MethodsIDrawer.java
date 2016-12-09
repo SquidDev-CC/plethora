4cd2e955beb99960116040de0a3393da04218c37
SquidDev <bonzoweb@hotmail.co.uk>
2016-12-09 12:11:45 +0000

    StorageDrawers integration
    See #15

---
+++ src/main/java/org/squiddev/plethora/integration/storagedrawers/MethodsIDrawer.java
@@ -0,0 +1,38 @@
+package org.squiddev.plethora.integration.storagedrawers;
+
+import com.jaquadro.minecraft.storagedrawers.StorageDrawers;
+import com.jaquadro.minecraft.storagedrawers.api.storage.IDrawer;
+import net.minecraft.item.ItemStack;
+import org.squiddev.plethora.api.method.BasicObjectMethod;
+import org.squiddev.plethora.api.method.IContext;
+
+public class MethodsIDrawer {
+	@BasicObjectMethod.Inject(
+		value = IDrawer.class, modId = StorageDrawers.MOD_ID, worldThread = true,
+		doc = "():int -- The maximum number of items in this drawer"
+	)
+	public static Object[] getCapacity(IContext<IDrawer> context, Object[] arguments) {
+		return new Object[]{context.getTarget().getMaxCapacity()};
+	}
+
+	@BasicObjectMethod.Inject(
+		value = IDrawer.class, modId = StorageDrawers.MOD_ID, worldThread = true,
+		doc = "():int -- The number of items in this drawer"
+	)
+	public static Object[] getCount(IContext<IDrawer> context, Object[] arguments) {
+		return new Object[]{context.getTarget().getStoredItemCount()};
+	}
+
+	@BasicObjectMethod.Inject(
+		value = IDrawer.class, modId = StorageDrawers.MOD_ID, worldThread = true,
+		doc = "():table -- The metadata of the item in this drawer"
+	)
+	public static Object[] getItemMeta(IContext<IDrawer> context, Object[] arguments) {
+		ItemStack stack = context.getTarget().getStoredItemPrototype();
+		return new Object[]{
+			stack == null
+				? null
+				: context.makePartialChild(stack).getMeta()
+		};
+	}
+}
---
+++ src/main/java/org/squiddev/plethora/integration/storagedrawers/MethodsIDrawerGroup.java
@@ -0,0 +1,44 @@
+package org.squiddev.plethora.integration.storagedrawers;
+
+import com.jaquadro.minecraft.storagedrawers.StorageDrawers;
+import com.jaquadro.minecraft.storagedrawers.api.storage.IDrawer;
+import com.jaquadro.minecraft.storagedrawers.api.storage.IDrawerGroup;
+import dan200.computercraft.api.lua.LuaException;
+import org.squiddev.plethora.api.method.*;
+
+import java.util.concurrent.Callable;
+
+import static org.squiddev.plethora.api.reference.Reference.id;
+
+public class MethodsIDrawerGroup {
+	@BasicObjectMethod.Inject(
+		value = IDrawerGroup.class, modId = StorageDrawers.MOD_ID, worldThread = true,
+		doc = "():int -- Return the number of drawers inside this draw group"
+	)
+	public static Object[] getDrawerCount(IContext<IDrawerGroup> context, Object[] arguments) {
+		return new Object[]{context.getTarget().getDrawerCount()};
+	}
+
+	@BasicMethod.Inject(
+		value = IDrawerGroup.class, modId = StorageDrawers.MOD_ID,
+		doc = "(slot:int):table -- Return the drawer at this particular slot"
+	)
+	public static MethodResult getDrawer(final IUnbakedContext<IDrawerGroup> context, Object[] args) throws LuaException {
+		final int slot = ArgumentHelper.getInt(args, 0);
+
+		return MethodResult.nextTick(new Callable<MethodResult>() {
+			@Override
+			public MethodResult call() throws Exception {
+				IContext<IDrawerGroup> baked = context.bake();
+				IDrawerGroup group = baked.getTarget();
+
+				ArgumentHelper.assertBetween(slot, 1, group.getDrawerCount(), "Index out of range (%s)");
+
+				IDrawer drawer = group.getDrawer(slot - 1);
+				if (drawer == null) return MethodResult.empty();
+
+				return MethodResult.result(baked.makeChild(id(drawer)).getObject());
+			}
+		});
+	}
+}
