34f5626505c3d7310b5f35174b4dfb8c850fe3fe
SquidDev <bonzoweb@hotmail.co.uk>
2017-02-26 18:48:03 +0000

    Add a load of additional renderers

--- src/main/java/org/squiddev/plethora/gameplay/client/RenderInterfaceLiving.java
+++ src/main/java/org/squiddev/plethora/gameplay/client/RenderInterfaceLiving.java
@@ -37,25 +37,32 @@ public class RenderInterfaceLiving extends Module implements IClientModule {
 		 * @see net.minecraft.entity.monster
 		 */
 		inject(EntityBlaze.class, 0, 3, 0);
-		inject(EntityCaveSpider.class, 0, 2, -3); // Same as normal spider
+		inject(EntityCaveSpider.class, 0, 2, -4);
 		inject(EntityCreeper.class, 0, -1, 0);
 		injectMonocle(EntityElderGuardian.class, -1, 20.5f, 4.2f, 0, 0, 0);
 		inject(EntityEnderman.class, 0, 0, 0);
 		injectMonocle(EntityEndermite.class, -1.5f, 5, 3.1f, 0, 0, 0);
+		inject(EntityEvoker.class, 0, 0, 0);
 		inject(EntityGhast.class, 1, 12, -4);
+		// EntityGiantZombie renders armor
 		injectMonocle(EntityGuardian.class, -1, 20.5f, 4.2f, 0, 0, 0);
 		inject(EntityIronGolem.class, 0, -2, -1);
 		inject(EntityMagmaCube.class, 0, 23, 0);
-		// PigZombie renders armor
+		// EntityPigZombie renders armor
 		inject(EntityPolarBear.class, 0, 3.5f, 1);
 		injectMonocle(EntityShulker.class, 0.1f, 7, 1, 0, 0, 0);
 		injectMonocle(EntitySilverfish.class, -0.5f, 4f, 3.1f, 0, 0, 0);
-		// Skeleton renders armor
+		// EntitySkeleton renders armor
 		inject(EntitySlime.class, 0, 23, 0); // This scales on size. I love it.
 		inject(EntitySnowman.class, 1, -3, -1);
-		inject(EntitySpider.class, 0, 2, -3);
+		inject(EntitySpider.class, 0, 2, -4);
+		// EntityStray renders armour
+		inject(EntityVex.class, 0, 0, 0);
+		inject(EntityVindicator.class, 0, 0, 0);
+		// EntityWitherSkeleton renders armour
 		inject(EntityWitch.class, 0, 0, 0);
-		// Zombie renders armor
+		// EntityZombie renders armor
+		// EntityZombieVillage renders armor
 
 		/**
 		 * @see net.minecraft.entity.passive
@@ -63,15 +70,20 @@ public class RenderInterfaceLiving extends Module implements IClientModule {
 		injectMonocle(EntityBat.class, 0, 4, 1, 0, 0, 0);
 		injectMonocle(EntityChicken.class, 0, -0.5f, 2, 0, 0, 0);
 		inject(EntityCow.class, 0, 4, -2);
+		injectMonocle(EntityDonkey.class, -1.5f, -4.5f, 0, 0, -90, 0);
 		injectMonocle(EntityHorse.class, -1.5f, -4.5f, 0, 0, -90, 0);
+		inject(EntityLlama.class, 0, -10, -2);
 		inject(EntityMooshroom.class, 0, 4, -2);
+		injectMonocle(EntityMule.class, -1.5f, -4.5f, 0, 0, -90, 0);
 		inject(EntityOcelot.class, -1, 3, 0);
 		inject(EntityPig.class, 0, 4, -4);
-		inject(EntityRabbit.class, 0, 1, -1);
+		injectMonocle(EntityRabbit.class, 0, 5, 1.5f, 0, 0, 0);
 		inject(EntitySheep.class, 0, 2, -2);
+		injectMonocle(EntitySkeletonHorse.class, -1.5f, -4.5f, 0, 0, -90, 0);
 		inject(EntitySquid.class, 2, 3, -2);
 		inject(EntityVillager.class, 0, 0, 0);
-		inject(EntityWolf.class, 0, 4, 3);
+		inject(EntityWolf.class, 0, 3, 2);
+		injectMonocle(EntityZombieHorse.class, -1.5f, -4.5f, 0, 0, -90, 0);
 
 		// Inject the baubles renderer
 		if (Loader.isModLoaded(Baubles.MODID)) {
@@ -136,7 +148,7 @@ public class RenderInterfaceLiving extends Module implements IClientModule {
 		} else if (model instanceof ModelEnderman) {
 			return ((ModelEnderman) model).bipedHeadwear;
 		} else if (model instanceof ModelGuardian) {
-			// guardianBody
+			// guardianEye
 			return ObfuscationReflectionHelper.getPrivateValue(ModelGuardian.class, (ModelGuardian) model, "field_178708_b");
 		} else if (model instanceof ModelRabbit) {
 			// rabbitHead
@@ -145,7 +157,7 @@ public class RenderInterfaceLiving extends Module implements IClientModule {
 			// slimeBodies
 			return ObfuscationReflectionHelper.getPrivateValue(ModelSlime.class, (ModelSlime) model, "field_78200_a");
 		} else if (model instanceof ModelMagmaCube) {
-			// code
+			// core
 			return ObfuscationReflectionHelper.getPrivateValue(ModelMagmaCube.class, (ModelMagmaCube) model, "field_78108_b");
 		} else if (model instanceof ModelSpider) {
 			return ((ModelSpider) model).spiderHead;
@@ -166,6 +178,10 @@ public class RenderInterfaceLiving extends Module implements IClientModule {
 			return renderers[0];
 		} else if (model instanceof ModelShulker) {
 			return ((ModelShulker) model).head;
+		} else if (model instanceof ModelIllager) {
+			return ((ModelIllager) model).head;
+		} else if (model instanceof ModelBiped) {
+			return ((ModelBiped) model).bipedHead;
 		} else {
 			return null;
 		}
