8fd582c6651112f420939069bdb3621ef9cbcc9d
SquidDev <bonzoweb@hotmail.co.uk>
2017-07-08 16:09:26 +0100

    Add support for ForgeMultipart too

---
+++ src/main/java/org/squiddev/plethora/integration/cbmp/MetaSlottedMultipart.java
@@ -0,0 +1,30 @@
+package org.squiddev.plethora.integration.cbmp;
+
+import codechicken.multipart.PartMap;
+import codechicken.multipart.TSlottedPart;
+import com.google.common.collect.Maps;
+import org.squiddev.plethora.api.meta.BasicMetaProvider;
+import org.squiddev.plethora.api.meta.IMetaProvider;
+
+import javax.annotation.Nonnull;
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Map;
+
+@IMetaProvider.Inject(value = TSlottedPart.class, modId = "forgemultipartcbe")
+public class MetaSlottedMultipart extends BasicMetaProvider<TSlottedPart> {
+	@Nonnull
+	@Override
+	public Map<Object, Object> getMeta(@Nonnull TSlottedPart object) {
+		int slots = object.getSlotMask();
+
+		int i = 0;
+		Map<Integer, String> out = Maps.newHashMapWithExpectedSize(Integer.bitCount(i));
+		for (PartMap slot : PartMap.values()) {
+			if ((slots & slot.mask) != 0) {
+				out.put(++i, slot.name().toLowerCase(Locale.ENGLISH));
+			}
+		}
+		return Collections.<Object, Object>singletonMap("slots", out);
+	}
+}
---
+++ src/main/java/org/squiddev/plethora/integration/cbmp/MethodsMultipart.java
@@ -0,0 +1,93 @@
+package org.squiddev.plethora.integration.cbmp;
+
+import codechicken.multipart.PartMap;
+import codechicken.multipart.TMultiPart;
+import codechicken.multipart.TileMultipart;
+import com.google.common.collect.Maps;
+import dan200.computercraft.api.lua.LuaException;
+import org.squiddev.plethora.api.method.*;
+
+import java.util.Collection;
+import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+import static org.squiddev.plethora.api.method.ArgumentHelper.getEnum;
+
+public class MethodsMultipart {
+	@BasicObjectMethod.Inject(
+		value = TileMultipart.class, modId = "forgemultipartcbe", worldThread = true,
+		doc = "function():table -- Get a list of all parts in the multipart."
+	)
+	public static Object[] listParts(IContext<TileMultipart> context, Object[] args) {
+		Collection<? extends TMultiPart> parts = context.getTarget().jPartList();
+
+		int i = 0;
+		Map<Integer, Map<Object, Object>> out = Maps.newHashMap();
+		for (TMultiPart part : parts) {
+			out.put(++i, MetaMultipart.getBasicMeta(part));
+		}
+
+		return new Object[]{out};
+	}
+
+	@BasicObjectMethod.Inject(
+		value = TileMultipart.class, modId = "forgemultipartcbe", worldThread = true,
+		doc = "function():table -- Get a lookup of slot to parts."
+	)
+	public static Object[] listSlottedParts(IContext<TileMultipart> context, Object[] args) {
+		Map<String, Map<Object, Object>> parts = Maps.newHashMap();
+
+		TileMultipart container = context.getTarget();
+		for (PartMap slot : PartMap.values()) {
+			TMultiPart part = container.partMap(slot.i);
+			if (part != null) {
+				parts.put(slot.name().toLowerCase(Locale.ENGLISH), MetaMultipart.getBasicMeta(part));
+			}
+		}
+
+		return new Object[]{parts};
+	}
+
+	@BasicMethod.Inject(
+		value = TileMultipart.class, modId = "forgemultipartcbe",
+		doc = "function(slot:string):table|nil -- Get a reference to the part in the specified slot."
+	)
+	public static MethodResult getSlottedPart(final IUnbakedContext<TileMultipart> context, Object[] args) throws LuaException {
+		final PartMap slot = getEnum(args, 0, PartMap.class);
+
+		return MethodResult.nextTick(new Callable<MethodResult>() {
+			@Override
+			public MethodResult call() throws Exception {
+				IContext<TileMultipart> baked = context.bake();
+				TileMultipart container = baked.getTarget();
+
+				TMultiPart part = container.partMap(slot.i);
+				return part == null
+					? MethodResult.empty()
+					: MethodResult.result(baked.makeChild(new ReferenceMultipart(container, part)).getObject());
+			}
+		});
+	}
+
+	@BasicMethod.Inject(
+		value = TileMultipart.class, modId = "forgemultipartcbe",
+		doc = "function(slot:string):table|nil -- Get the metadata of the part in the specified slot."
+	)
+	public static MethodResult getSlottedPartMeta(final IUnbakedContext<TileMultipart> context, Object[] args) throws LuaException {
+		final PartMap slot = getEnum(args, 0, PartMap.class);
+
+		return MethodResult.nextTick(new Callable<MethodResult>() {
+			@Override
+			public MethodResult call() throws Exception {
+				IContext<TileMultipart> baked = context.bake();
+				TileMultipart container = baked.getTarget();
+
+				TMultiPart part = container.partMap(slot.i);
+				return part == null
+					? MethodResult.empty()
+					: MethodResult.result(baked.makePartialChild(part).getMeta());
+			}
+		});
+	}
+}
