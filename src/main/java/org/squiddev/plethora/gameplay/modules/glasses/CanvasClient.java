568db44b9ec46dde01f7365e9d5d09a2cc598804
SquidDev <bonzoweb@hotmail.co.uk>
2018-01-21 15:46:50 +0000

    Use a tree map instead of hash map for canvas objects
    This means they are rendered by ID, and so by insertion order.
    This means objects are drawn on top of each other in a deterministic
    order.

--- src/main/java/org/squiddev/plethora/gameplay/modules/glasses/CanvasClient.java
+++ src/main/java/org/squiddev/plethora/gameplay/modules/glasses/CanvasClient.java
@@ -1,11 +1,12 @@
 package org.squiddev.plethora.gameplay.modules.glasses;
 
-import gnu.trove.map.hash.TIntObjectHashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class CanvasClient {
 	public final int id;
 
-	public final TIntObjectHashMap<BaseObject> objects = new TIntObjectHashMap<BaseObject>();
+	public final Map<Integer, BaseObject> objects = new TreeMap<Integer, BaseObject>();
 
 	public CanvasClient(int id) {
 		this.id = id;
